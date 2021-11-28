823ceb1ddb4359884d3ac79c39fb1ce4248f916d
Wiwid <HP-ACT@DESKTOP-HVOF19J>
2021-11-28 20:20:34 +0700

    fix right side

--- dummy_app/lib/main.dart
+++ dummy_app/lib/main.dart
@@ -13,98 +13,95 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Container(
-            color: Colors.lightBlue,
-            padding: EdgeInsets.only(top: 200),
-            
-            child: Container(
-              padding: EdgeInsets.only(top:50),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(30),
-                  topRight: Radius.circular(30),
-                ),
-                color: Colors.white,
-              ),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  Column(
-                    children: [
-                      Text(
-                        "Welcome to Flutter",
-                        style: TextStyle(
-                          fontSize: 30,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.black,
-                        ),
-                      ),
-                      SizedBox(
-                        height: 20,
-                      ),
-                      Container(
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(20),
-                          color: Colors.red,
-                        ),
-                        width: 315,
-                        height: 50,
-                        
-                      ),
-                    ],
+          body: Container(
+            width: MediaQuery.of(context).size.width,
+        color: Colors.lightBlue,
+        padding: EdgeInsets.only(top: 200),
+        child: Container(
+          padding: EdgeInsets.only(top: 50),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(30),
+              topRight: Radius.circular(30),
+            ),
+            color: Colors.white,
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Column(
+                children: [
+                  Text(
+                    "Welcome to Flutter",
+                    style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
                   ),
-                  Column(
-                    children: [
-                      Text(
-                        "Welcome to Flutter",
-                        style: TextStyle(
-                          fontSize: 30,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.black,
-                        ),
-                      ),
-                      SizedBox(
-                        height: 20,
-                      ),
-                      Container(
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(20),
-                          color: Colors.red,
-                        ),
-                        width: 315,
-                        height: 50,
-                        
-                      ),
-                    ],
+                  SizedBox(
+                    height: 20,
                   ),
-                  Column(
-                    children: [
-                      Text(
-                        "Welcome to Flutter",
-                        style: TextStyle(
-                          fontSize: 30,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.black,
-                        ),
-                      ),
-                      SizedBox(
-                        height: 20,
-                      ),
-                      Container(
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(20),
-                          color: Colors.lightBlue,
-                        ),
-                        width: 315,
-                        height: 50,
-                        
-                      ),
-                    ],
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: Colors.red,
+                    ),
+                    width: 315,
+                    height: 50,
                   ),
                 ],
               ),
-            ),
-            )),
-      );
+              Column(
+                children: [
+                  Text(
+                    "Welcome to Flutter",
+                    style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: Colors.red,
+                    ),
+                    width: 315,
+                    height: 50,
+                  ),
+                ],
+              ),
+              Column(
+                children: [
+                  Text(
+                    "Welcome to Flutter",
+                    style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: Colors.lightBlue,
+                    ),
+                    width: 315,
+                    height: 50,
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      )),
+    );
   }
 }
